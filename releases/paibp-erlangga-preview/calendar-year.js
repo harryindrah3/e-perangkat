188f0c65ef33b4d8a8177e571a1b82b16bebd1f6
harryindrah3 <36720512+harryindrah3@users.noreply.github.com>
2026-09-24 06:23:43 +0800

    feat(preview): add calendar-year.js from PAIBP umum

---
+++ releases/paibp-erlangga-preview/calendar-year.js
@@ -0,0 +1,8 @@
+(() => {
+  'use strict';
+  const STABLE = 'https://e-perangkat-online-a-6xpm8q2nz-harryindrah3-6239s-projects.vercel.app/calendar-year.js?v=20260914-1';
+  document.write(
+    '<scr' + 'ipt src="' + STABLE + '"></scr' + 'ipt>' +
+    '<scr' + 'ipt src="/calendar-2024-extension.js?v=20260917-1"></scr' + 'ipt>'
+  );
+})();
